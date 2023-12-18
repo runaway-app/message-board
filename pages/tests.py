1da55ed00122faee3b5ddee95012603ed170f4ac
shiblibaig <shiblibaig01@gmail.com>
2023-12-18 22:03:32 +0530

    second commit

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase 
+from .models import Posts
 
-# Create your tests here.
+class PostTests(TestCase): 
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Posts.objects.create(text="This is a test!")
+    
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
